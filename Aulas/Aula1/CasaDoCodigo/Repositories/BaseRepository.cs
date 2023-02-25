8d866ed15f51a3bda86e45d3d82480e870f80fc0
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-02-25 13:51:31 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepository<T> where T : BaseModel
+    {
+        protected readonly ApplicationContext contexto;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+            dbSet = contexto.Set<T>();
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    {
+
+    }
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
