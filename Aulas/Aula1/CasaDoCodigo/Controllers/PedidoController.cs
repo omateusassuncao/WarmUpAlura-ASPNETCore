4adcd799de8214030f1550d68df22609e5d1f454
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-03-12 10:14:19 -0300

    Validando o formulário e resumo

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -45,13 +45,21 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if(pedido == null) return RedirectToAction("Carrossel");
+
+            return View(pedido.Cadastro);
         }
 
-        public IActionResult Resumo()
+        [HttpPost]
+        public IActionResult Resumo(Cadastro cadastro)
         {
-            Pedido pedido = pedidoRepository.GetPedido();
-            return View(pedido);
+            if (ModelState.IsValid)
+            {
+                return View(pedidoRepository.UpdateCadastro(cadastro));
+            }
+            return RedirectToAction("Cadastro");
         }
 
         [HttpPost]
--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -14,6 +14,7 @@ namespace CasaDoCodigo.Repositories
         Pedido GetPedido();
         void AddItem(string codigo);
         UpdateQuantidadeResponse UpdateQuantidade(ItemPedido itemPedido);
+        Pedido UpdateCadastro(Cadastro cadastro);
 
     }
 
@@ -21,13 +22,16 @@ namespace CasaDoCodigo.Repositories
     {
         private readonly IHttpContextAccessor contextAccessor;
         private readonly IItemPedidoRepository itemPedidoRepository;
+        private readonly ICadastroRepository cadastroRepository;
 
         public PedidoRepository(ApplicationContext contexto,
             IHttpContextAccessor contextAccessor,
-            IItemPedidoRepository itemPedidoRepository) : base(contexto)
+            IItemPedidoRepository itemPedidoRepository,
+            ICadastroRepository cadastroRepository) : base(contexto)
         {
             this.contextAccessor = contextAccessor;
             this.itemPedidoRepository = itemPedidoRepository;
+            this.cadastroRepository = cadastroRepository;
         }
 
         public void AddItem(string codigo)
@@ -106,6 +110,13 @@ namespace CasaDoCodigo.Repositories
             }
 
             throw new ArgumentException("Item pedido não encontrado.");
+        }
+
+        public Pedido UpdateCadastro(Cadastro cadastro)
+        {
+            var pedido = GetPedido();
+            cadastroRepository.Update(pedido.Cadastro.Id, cadastro);
+            return pedido;
         }
     }
 }
